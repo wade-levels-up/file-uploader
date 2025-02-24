c9ce199e2e2151bf321085834465bca9e1085716
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-02-24 16:22:21 +1100

    Add basic user sign-up form to homepage
    The EJS index template will now render differently
    depending on whether a user us logged in or not.

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -2,7 +2,7 @@ const asyncHandler = require("express-async-handler");
 
 const getIndexPage = asyncHandler(async (req, res) => {
   try {
-    res.render("pages/index", { title: "Index" });
+    res.render("pages/index", { title: "Index", user: req.user });
   } catch (error) {
     throw new Error(`Couldn't get the index page ${error}`);
   }
