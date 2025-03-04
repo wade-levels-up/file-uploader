f847b88e84d092e58fa3dcc71add956117c22057
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-03-04 18:31:56 +1100

    Commit prior to removing destination field

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -2,7 +2,11 @@ const asyncHandler = require("express-async-handler");
 
 const getIndexPage = asyncHandler(async (req, res) => {
   try {
-    res.render("pages/index", { user: req.user });
+    if (req.user) {
+      res.render("pages/index", { user: req.user });
+    } else {
+      res.render("pages/index");
+    }
   } catch (error) {
     throw new Error(`Couldn't get the index page ${error}`);
   }
